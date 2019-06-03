1e1e1853e95fd6412a425d9ea3116fbbfeeac24d
zhoujia <zhou.jia@jyall.com>
2019-06-03 11:03:19 +0800

    数据排序

--- src/main/java/com/yingchong/service/data_service/mapper/MyAppTypeMapper.java
+++ src/main/java/com/yingchong/service/data_service/mapper/MyAppTypeMapper.java
@@ -26,7 +26,7 @@ public interface MyAppTypeMapper {
     @Select("select sum(action_count) num,action_name app,action_date date \n" +
             "from action_type \n" +
             "where action_date >= #{startTime} and action_date <= #{endTime} and action_name = #{appName} \n" +
-            "GROUP BY action_date ORDER BY num desc")
+            "GROUP BY action_date ORDER BY action_date")
     List<BizAppTypeBean> selectAppTypeTreadResultByAppName(
             @Param("startTime") String startTime,
             @Param("endTime") String endTime,
