489b02d2cdb18a70cf04a4eee513d5f9dd10d251
zhoujia <zhou.jia@jyall.com>
2019-06-12 16:11:53 +0800

    shart.sh

--- start.sh
+++ start.sh
@@ -9,4 +9,5 @@ gradle build -x test
 JAVA_OPTS="-Xms2048M -Xmx2048M -Xmn1024M -Xss128m -XX:+DisableExplicitGC -XX:MaxTenuringThreshold=15 -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSParallelRemarkEnabled -XX:LargePageSizeInBytes=128m -XX:CMSInitiatingOccupancyFraction=75 -Duser.timezone=Asia/Shanghai"
 #kill -9 $(netstat -nlp | grep :8000 | awk '{print $7}' | awk -F"/" '{ print $1 }')
 pkill -f data-service
+sleep 5
 nohup java $JAVA_OPTS -jar build/libs/data-service-0.0.1-SNAPSHOT.jar --spring.profiles.active=$ENV >/dev/null 2>&1 &
